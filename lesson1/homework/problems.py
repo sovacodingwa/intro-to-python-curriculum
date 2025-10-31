2c2db0d89a96a97c1cd58c1ccd64614d4e6e9e6f
dakshc1807-sys <dakshc1807@gmail.com>
2025-10-30 17:33:09 -0700

    daksh-class1-homework

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,33 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
-
+myLuckyNumber = 7
+print(myLuckyNumber)
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
+numbersOfHoursISleep = 9
+print(numbersOfHoursISleep)
 
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
-
+favoriteFruit = "apple"
+print("My favorite fruit is " + favoriteFruit)
 
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
-
+city = "Sammamish"
+country = "United States Of America"
+print(city)
+print(country)
+      
 
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+petName = "Bruno" 
+petAge = 12
+petType = "Poodle" 
+print("My pet's name is " + petName + ", his age is " + str(petAge) + ", he is a " + petType + ".")
